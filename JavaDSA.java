80174a37448bd43318c698d938b95ad136b7686f
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-05-29 17:12:02 +0530

    Stack : using stack class

--- JavaDSA.java
+++ JavaDSA.java
@@ -1,79 +1,34 @@
-class JavaDSA{
-    private int maxSize;
-    private int top;
-    private int[] stackArray;
+/**
+ * stack
+ */
+// Built in stack class to perform stack operations
+import java.util.Stack;
 
-    public JavaDSA(int size){
-        maxSize = size;
-        stackArray = new int[maxSize];
-        top=-1;
-    }
-    
-    // Insert element in stack
-    public void push(int value){
-        if(top==maxSize-1){
-            System.out.println("Stack is full, you can not push element " + value);
-        }
-        else{
-            top++;
-            stackArray[top]=value;
-            System.out.println("Pushed element is " + value);
-        }
-    }
+public class JavaDSA {
+    public static void main(String args []){
+        Stack <Integer> st = new Stack<>();
 
-    // remove element in stack
-    public void pop(){
-        if(top==-1){
-            System.out.println("Stack is empty we can't pop element");
-        }
-        else{
-            int value = stackArray[top];
-            top--;
-            System.out.println("Poped element is " + value);
-            // return value;
-        }
-    }
+        // Insert element in stack
+        st.push(10);
+        st.push(20);
+        st.push(30);
+        st.push(40);
+        st.push(50);
+        System.out.println("Stack Elements are " + st);
 
-    // Display the top element of stack
-    public int peek(){
-        if(top==-1){
-            System.out.println("Stack is empty cannot peek element");
-            return -1;
-        }
-        else return stackArray[top];
-    }
+        // Top element
+        int topElement = st.peek();
+        System.out.println(topElement);
+        System.out.println("Stack Element after peek() "+ st);
 
-    // Check stack is empty
-    public boolean isEmpty(){
-        return (top==-1);
-    }
+        // Remove top element
+        int popElement = st.pop();
+        System.out.println("Removed element is "+ popElement);
+        System.out.println("Stack after pop() "+st);
 
-    // Check stack is full
-    public boolean isFull(){
-        return (top==maxSize-1);
-    }
+        // isEmpty
+        System.out.println("Stack is empty " + st.isEmpty());
 
-    public void display(){
-        for(int i=top; i>=0; i--){
-            System.out.print(stackArray[i] + " ");
-        }
-        System.out.println();
-    }
-    public static void main(String[] args) {
-        JavaDSA obj = new JavaDSA(5);
-        obj.push(10);
-        obj.push(20);
-        obj.push(30);
-        obj.push(40);
-        obj.push(50);
-        obj.push(60);
 
-        obj.display();
-        obj.pop();
-        obj.display();
-
-        System.out.println("Top element is " + obj.peek());
-        System.out.println("Is stack empty " + obj.isEmpty());
-        System.out.println("Is stack full " + obj.isFull());
     }
 }
