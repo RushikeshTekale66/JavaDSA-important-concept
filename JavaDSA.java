5fe140cd7294d14da0e3342c3ac7510a87af4873
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-06-05 17:38:10 +0530

    Linked List : Basic linkedlist

--- JavaDSA.java
+++ JavaDSA.java
@@ -1,102 +1,48 @@
-/**
- * JavaDSA
- */
-public class JavaDSA {
-    int size = 5;
-    int items[] = new int [size];
-    int front, rear;
+class JavaDSA{
+    private Node head;
 
-    JavaDSA(){
-        front = -1;
-        rear = -1;
-    }
+    private static class Node{
+        private int data;
+        private Node next;
 
-    // check queue is full 
-    boolean isFull(){
-        if(front ==0 && rear==size-1){
-            return true;
+        public Node(int data){
+            this.data = data;
+            this.next = null;
         }
-        return false;
     }
 
-    // check queue is empty
-    boolean isEmpty(){
-        if(front==-1){
-            return true;
-        }
-        return false;
-    }
+    // Insert the node
+    public void insert(int data){
+        Node newNode = new Node(data); //new node creation
 
-    // Insert element in queue
-    void enqueue(int item){
-        if(isFull()){
-            System.out.println("Queue is full - Overflow condition, Can't Insert " + item);
+        if(head==null){
+            head = newNode;
         }
         else{
-            if(front==-1){
-                front=0;
+            Node current = head;
+            while(current.next!=null){
+                current = current.next;
             }
-            rear++;
-            items[rear]=item;
-            System.out.println("Inserted : "+item);
+            current.next = newNode;
         }
     }
 
-    // delete element from queue
-    void dequeue(){
-        int val ;
-        if(isEmpty()){
-            System.out.println("Queue is empty - Underflow conditin");
-        }
-        else{
-            val= items[front];
-            if(front>=rear){
-                front=-1;
-                rear=-1;
-            }
-            else{
-                front++;
-            }
-            System.out.println("Deleted element is  "+ val);
+    // Display the linked list
+    public void display(){
+        Node current = head;
+        while(current!=null){
+            System.out.print(current.data+"  ");
+            current = current.next;
         }
     }
 
-    // Print queue
-    void display(){
-        int i;
-        if(isEmpty()){
-            System.out.println("Queue is empty");
-        }
-        else{
-            for(i=front; i<=rear; i++){
-                System.out.print(items[i]+ " ");
-            }
-        }
-    }
-
-    public static void main(String args []){
-        JavaDSA q = new JavaDSA();
-        q.dequeue();
-
-        q.enqueue(10);
-        q.enqueue(20);
-        q.enqueue(30);
-
-        q.dequeue();
-
-        q.enqueue(40);
-        q.enqueue(50);
-
-        q.display();
-
-        q.dequeue();
-        q.display();
-
-        q.dequeue();
-        q.display();
-
-        q.dequeue();
-        q.display();
+    public static void main(String [] args){
+        JavaDSA obj = new JavaDSA();
+        obj.insert(10);
+        obj.insert(20);
+        obj.insert(30);
+        obj.insert(40);
+        obj.display();
 
     }
 }
