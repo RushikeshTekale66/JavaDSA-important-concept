2fdfbbce725ec7cf14647cc23a3817903831b02b
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-06-05 18:27:05 +0530

    Linked List : Insertion & Deletion in Simple linked list

--- JavaDSA.java
+++ JavaDSA.java
@@ -1,48 +1,186 @@
-class JavaDSA{
+import javax.management.RuntimeErrorException;
+
+class JavaDSA {
     private Node head;
 
-    private static class Node{
+    private static class Node {
         private int data;
         private Node next;
 
-        public Node(int data){
+        public Node(int data) {
             this.data = data;
             this.next = null;
         }
     }
 
-    // Insert the node
-    public void insert(int data){
-        Node newNode = new Node(data); //new node creation
+    // 1) Insertion Operations
+    // Insert the node at bigining
+    public void insertAtBigning(int data) {
+        Node newNode = new Node(data); // new node creation
+        newNode.next = head;
+        head = newNode;
+        System.out.println("Inserted at begening " + data);
+    }
 
-        if(head==null){
+    // Insert at end
+    public void insertAtEnd(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
             head = newNode;
-        }
-        else{
+        } else {
             Node current = head;
-            while(current.next!=null){
+            while (current.next != null) {
                 current = current.next;
             }
             current.next = newNode;
         }
+        System.out.println("Inserted at end " + data);
+    }
+
+    // Insert at middle
+    public void insertAtIndex(int index, int data) {
+
+        // Throw error if index is less than zero or index is greater then the size of
+        // linkedlist
+        if (index < 0 || index > getSize()) {
+            throw new IndexOutOfBoundsException("Invalid index");
+        }
+
+        // Insert index at begining
+        if (index == 0) {
+            insertAtBigning(data);
+        }
+        // Insert at end
+        else if (index == getSize()) {
+            insertAtEnd(data);
+        }
+        // Insert at given index
+        else {
+            Node newNode = new Node(data);
+            Node current = head;
+            for (int i = 0; i < index; i++) {
+                current = current.next;
+            }
+            newNode.next = current.next;
+            current.next = newNode;
+        }
+        System.out.println("Inserted at index " + index + " : "+ data);
+    }
+
+    // 2) Deletion Operation
+    // Delete at begining
+    public void deleteAtBegining() {
+        if (head == null) {
+            throw new RuntimeException("Linked list is empty");
+        }
+        head = head.next;
+    }
+
+    // Delete at end
+    public void deleteAtEnd() {
+        // If Linked list is empty
+        if (head == null) {
+            throw new RuntimeException("Linked list is empty");
+        }
+        // If liked list have only 1 element
+        else if (head.next == null) {
+            head = null;
+        } else {
+            Node current = head;
+            while (current.next.next != null) {
+                current = current.next;
+            }
+            current.next = null;
+        }
+        System.out.println("Deleted at end");
+    }
+
+    // Delete at perticular index
+    public void deleteAtIndex(int index) {
+        // Throw error if index is less than zero or index is greater then the size of
+        // linkedlist
+        if (index < 0 || index > getSize()) {
+            throw new IndexOutOfBoundsException("Invalid index");
+        }
+
+        // delete at begining
+        if(index==0){
+            deleteAtBegining();
+        }
+        // delete at end
+        else if (index==getSize()){
+            deleteAtEnd();
+        }
+        else {
+            Node current = head;
+            for(int i=0; i<index-1; i++){
+                current = current.next;
+            }
+            current.next = current.next.next;
+        }
+        System.out.println("Deleted at index "+index);
+
+    }
+
+    // getting the size of the linked list
+    public int getSize(){
+        int count =0; 
+        Node current = head;
+        while(current !=null){
+            count++;
+            current = current.next;
+        }
+        return count;
+    }
+
+    // check the linked list is empty or not
+    public void isEmpty(){
+        if(head==null){
+            System.out.println("List is empty");
+        }
+        else{
+            System.out.println("List have some element");
+        }
+        
     }
 
     // Display the linked list
-    public void display(){
+    public void display() {
         Node current = head;
-        while(current!=null){
-            System.out.print(current.data+"  ");
+        if(head==null) isEmpty();
+        while (current != null) {
+            System.out.print(current.data + "  ");
             current = current.next;
         }
+        System.out.println();
     }
 
-    public static void main(String [] args){
+    public static void main(String[] args) {
         JavaDSA obj = new JavaDSA();
-        obj.insert(10);
-        obj.insert(20);
-        obj.insert(30);
-        obj.insert(40);
+
         obj.display();
+
+        obj.insertAtBigning(10);
+        obj.insertAtBigning(20);
+        obj.insertAtBigning(30);
+        obj.insertAtBigning(40);
+
+        obj.display();
+
+        obj.insertAtEnd(100);
+        obj.display();
+
+        obj.insertAtIndex(2, 200);
+        obj.display();
+
+        obj.deleteAtBegining();
+        obj.display();
+
+        obj.deleteAtEnd();
+        obj.display();
+
+        obj.deleteAtIndex(2);
 
     }
 }
