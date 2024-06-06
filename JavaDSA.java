f86fdaed3038b5e01884d6dcced64dc469bfc258
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-06-06 18:05:58 +0530

    Linked List : circular linked list

--- JavaDSA.java
+++ JavaDSA.java
@@ -1,8 +1,9 @@
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 class JavaDSA {
     public static void main(String[] args) {
-        LinkedList<Integer> obj = new LinkedList<>();
+        LinkedList <Integer> obj = new LinkedList<>();
 
         obj.add(10);
         obj.add(20);
@@ -10,16 +11,18 @@ class JavaDSA {
         obj.add(40);
         obj.add(50);
 
-        System.out.println("Linked list is " + obj);
+        ListIterator <Integer> iterator = obj.listIterator();
 
-        obj.addFirst(100);
-        System.out.println("Linked List after adding first : " + obj);
+        while(iterator.hasNext()){
+            System.out.print(iterator.next()+" ");
+        }
+        System.out.println();
 
-        obj.addLast(200);
-        System.out.println("Linked List after adding last : " + obj);
+        obj.addFirst(obj.removeLast());
+        iterator = obj.listIterator();
 
-        obj.removeFirst();
-        obj.removeLast();
-        System.out.println("Linked list after removing first & last : " + obj);
+        for(int i=0; i<obj.size(); i++){
+            System.out.print(iterator.next() + " ");
+        }
     }
 }
