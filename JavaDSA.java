c8ba5ab2d71bb7ff2d8b112e4be791bc513a2504
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-06-12 09:38:39 +0530

    Hashmap class

--- JavaDSA.java
+++ JavaDSA.java
@@ -1,134 +1,31 @@
+import java.util.Map;
+import java.util.HashMap;
+
+/**
+ * JavaDSA
+ */
 public class JavaDSA {
 
-    private Entry[] table;
-    private int capacity;
-
-    private class Entry {
-
-        private String key;
-        private int value;
-        private Entry next;
-
-        public Entry(String key, int value) {
-            this.key = key;
-            this.value = value;
-            this.next = null;
-        }
-    }
-
-    public JavaDSA(int capacity) {
-        this.capacity = capacity;
-        this.table = new Entry[capacity];
-    }
-
-    private int hash(String key) {
-        int hash = key.hashCode();
-        return Math.abs(hash) % capacity;
-    }
-
-    public void put(String key, int value) {
-        int index = hash(key);
-
-        Entry entry = table[index];
-
-        while (entry != null) {
-            if (entry.key.equals(key)) {
-                entry.value = value;
-                return;
-            }
-            entry = entry.next;
-        }
-        Entry newEntry = new Entry(key, value);
-        newEntry.next = table[index];
-        table[index] = newEntry;
-    }
-
-    public int get(String key) {
-        int index = hash(key);
-        Entry entry = table[index];
-
-        while (entry != null) {
-            if (entry.key.equals(key)) {
-                return entry.value;
-            }
-            entry = entry.next;
-        }
-        return -1;
-    }
-
-    public void remove(String key) {
-        int index = hash(key);
-        Entry entry = table[index];
-        Entry prev = null;
-
-        while (entry != null) {
-            if (entry.key.equals(key)) {
-                if (prev == null) {
-                    table[index] = entry.next;
-                } else {
-                    prev.next = entry.next;
-                }
-                return;
-
-            }
-            prev = entry;
-            entry = entry.next;
-        }
-    }
-
-    public boolean containsKey(String key){
-        int index = hash(key);
-
-        Entry entry = table[index];
-
-        while (entry!=null) {
-            if(entry.key.equals(key)){
-                return true;
-            }
-            entry = entry.next;
-            
-        }
-        return false;
-    }
-
-    public void display(){
-        for(int i=0; i<capacity; i++){
-            Entry entry = table[i];
-
-            while (entry!=null) {
-                System.out.println("Key: "+ entry.key + ", Value : "+ entry.value);
-                entry  = entry.next;
-            }
-        }
-    }
-
     public static void main(String [] args){
-        JavaDSA ht = new JavaDSA(10) ;
+        Map<String, Integer> ht = new HashMap<>();
 
-        ht.put("banana", 8);
-        ht.put("orange", 6);
+        // Add key value to hashmap
         ht.put("apple", 5);
+        ht.put("banana", 8);
+        ht.put("orange", 3);
 
-        ht.display();
+        // get the value of that key
+        int appleCount = ht.get("apple");
+        System.out.println("Count of apple is : "+ appleCount);
 
-        int applecount = ht.get("apple");
-        int bananacount = ht.get("banana");
-        int orangecount = ht.get("orange");
+        System.out.println(ht);
 
-        System.out.println("Number of apples : " + applecount);
-        System.out.println("Number of banana : " + bananacount);
-        System.out.println("Number of orange : " + orangecount);
-
-        ht.put("banana", 10);
-
+        // remove the data as per key
         ht.remove("orange");
+        System.out.println(ht);
 
-        boolean containsapple = ht.containsKey("apple");
-        System.out.println("Containes apple : "+ containsapple);
-
-        boolean containsgreaps = ht.containsKey("greaps");
-        System.out.println("Contains greaps : "+ containsgreaps);
-
-        ht.display();
+        // Check the hashmap contains the key
+        boolean containsApple = ht.containsKey("apple");
+        System.out.println("Contains apple ? "+containsApple);
     }
 }
