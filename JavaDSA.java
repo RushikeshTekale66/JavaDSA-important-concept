417035351b136e92e8dd6d2c008b476392c0eb9e
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-06-12 08:56:39 +0530

    HashTable : without HashMap class

--- JavaDSA.java
+++ JavaDSA.java
@@ -1,60 +1,134 @@
-
-// Doubly Linked list
-class Node {
-    int data ;
-    Node prev;
-    Node next;
-
-    public Node(int data){
-        this.data = data;
-        this.prev = null;
-        this.next = null;
-    }
-}
-
 public class JavaDSA {
 
-     Node head;
-     public JavaDSA(){
-        head = null;
-     }
+    private Entry[] table;
+    private int capacity;
 
-    //  Insert at begning
-     public void insert(int data){
-        Node newNode = new Node(data);
+    private class Entry {
 
-        if(head ==null){
-            head = newNode;
+        private String key;
+        private int value;
+        private Entry next;
+
+        public Entry(String key, int value) {
+            this.key = key;
+            this.value = value;
+            this.next = null;
         }
-        else{
-            newNode.next=head;
-            head.prev=newNode;
-            head=newNode;
-        }
-     }
+    }
 
-    //  Display list
+    public JavaDSA(int capacity) {
+        this.capacity = capacity;
+        this.table = new Entry[capacity];
+    }
+
+    private int hash(String key) {
+        int hash = key.hashCode();
+        return Math.abs(hash) % capacity;
+    }
+
+    public void put(String key, int value) {
+        int index = hash(key);
+
+        Entry entry = table[index];
+
+        while (entry != null) {
+            if (entry.key.equals(key)) {
+                entry.value = value;
+                return;
+            }
+            entry = entry.next;
+        }
+        Entry newEntry = new Entry(key, value);
+        newEntry.next = table[index];
+        table[index] = newEntry;
+    }
+
+    public int get(String key) {
+        int index = hash(key);
+        Entry entry = table[index];
+
+        while (entry != null) {
+            if (entry.key.equals(key)) {
+                return entry.value;
+            }
+            entry = entry.next;
+        }
+        return -1;
+    }
+
+    public void remove(String key) {
+        int index = hash(key);
+        Entry entry = table[index];
+        Entry prev = null;
+
+        while (entry != null) {
+            if (entry.key.equals(key)) {
+                if (prev == null) {
+                    table[index] = entry.next;
+                } else {
+                    prev.next = entry.next;
+                }
+                return;
+
+            }
+            prev = entry;
+            entry = entry.next;
+        }
+    }
+
+    public boolean containsKey(String key){
+        int index = hash(key);
+
+        Entry entry = table[index];
+
+        while (entry!=null) {
+            if(entry.key.equals(key)){
+                return true;
+            }
+            entry = entry.next;
+            
+        }
+        return false;
+    }
+
     public void display(){
-        if(head==null){
-            System.out.println("List is empty");
-        }
-        else{
-            Node current = head;
-            while (current!=null) {
-                System.out.println(current.data + " ");
-                current=current.next;
+        for(int i=0; i<capacity; i++){
+            Entry entry = table[i];
+
+            while (entry!=null) {
+                System.out.println("Key: "+ entry.key + ", Value : "+ entry.value);
+                entry  = entry.next;
             }
         }
     }
 
     public static void main(String [] args){
-        JavaDSA obj = new JavaDSA();
+        JavaDSA ht = new JavaDSA(10) ;
 
-        obj.insert(10);
-        obj.insert(20);
-        obj.insert(30);
-        obj.insert(40);
+        ht.put("banana", 8);
+        ht.put("orange", 6);
+        ht.put("apple", 5);
 
-        obj.display();
+        ht.display();
+
+        int applecount = ht.get("apple");
+        int bananacount = ht.get("banana");
+        int orangecount = ht.get("orange");
+
+        System.out.println("Number of apples : " + applecount);
+        System.out.println("Number of banana : " + bananacount);
+        System.out.println("Number of orange : " + orangecount);
+
+        ht.put("banana", 10);
+
+        ht.remove("orange");
+
+        boolean containsapple = ht.containsKey("apple");
+        System.out.println("Containes apple : "+ containsapple);
+
+        boolean containsgreaps = ht.containsKey("greaps");
+        System.out.println("Contains greaps : "+ containsgreaps);
+
+        ht.display();
     }
 }
