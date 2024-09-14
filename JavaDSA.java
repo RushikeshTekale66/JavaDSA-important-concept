5c968957db40d565e20b60415189f7f676d603e1
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-09-14 10:46:32 +0530

    Insertion sort

--- JavaDSA.java
+++ JavaDSA.java
@@ -1,16 +1,15 @@
 class JavaDSA {
 
-    public void selectionSort(int[] arr) {
-        for (int i = 0; i < arr.length - 1; i++) {
-            int minIndex = i;
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[j] < arr[minIndex]) {
-                    minIndex = j;
-                }
+    public void insertionSort(int[] arr) {
+        for(int i=0; i<arr.length; i++){
+            int key = arr[i];
+            int j=i-1;
+
+            while(j>=0 && arr[j]>key){
+                arr[j+1] = arr[j];
+                j=j-1;
             }
-            int temp = arr[minIndex];
-            arr[minIndex] = arr[i];
-            arr[i] = temp;
+            arr[j+1] = key;
         }
     }
 
@@ -24,7 +23,7 @@ class JavaDSA {
         int[] arr = { 1, 3, 4, 2, 5 };
 
         JavaDSA jd = new JavaDSA();
-        jd.selectionSort(arr);
+        jd.insertionSort(arr);
         jd.display(arr);
     }
 }
