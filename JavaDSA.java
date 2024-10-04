6bd58003731cfc3f727bb623b4def5128c2ce53f
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-10-04 20:37:58 +0530

    Radix sort

--- JavaDSA.java
+++ JavaDSA.java
@@ -1,19 +1,26 @@
 import java.util.Arrays;
-class JavaDSA{
-    public static void counting(int [] arr){
+
+/**
+ * JavaDSA
+ */
+public class JavaDSA {
+
+    public static void radixSort(int [] arr){
         int max = Arrays.stream(arr).max().getAsInt();
-        int [] count = new int[max+1];
-
-        for(int num : arr) count[num]++;
-
-        for(int i=0, index = 0; i<=max; i++){
-            while(count[i]-->0) arr[index++]=i;
+        for(int exp = 1; max/exp>0; exp*=10){
+            int [] output = new int[arr.length];
+            int[] count = new int[10];
+            
+            for(int num:arr) count[(num/exp)%10]++;
+            for(int i=1; i<10; i++) count[i]+=count[i-1];
+            for(int i=arr.length-1; i>=0; i--) output[--count[(arr[i]/exp)%10]]=arr[i];
+            System.arraycopy(output, 0, arr, 0, arr.length);
         }
     }
-    public static void main(String [] args){
-        int [] arr = {4, 3, 2, 1, 4, 3, 2,1, 1, 2, 3, 4, 5};
 
-        counting(arr);
+    public static void main(String [] args){
+        int [] arr = {180, 90, 23, 89, 1909};
+        radixSort(arr);
         System.out.println(Arrays.toString(arr));
     }
 }
