faf54cb1f217049f57219a07871ebdd2f6c6b888
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-10-04 19:45:34 +0530

    Count Sort

--- JavaDSA.java
+++ JavaDSA.java
@@ -1,47 +1,19 @@
 import java.util.Arrays;
+class JavaDSA{
+    public static void counting(int [] arr){
+        int max = Arrays.stream(arr).max().getAsInt();
+        int [] count = new int[max+1];
 
-class JavaDSA {
+        for(int num : arr) count[num]++;
 
-    public void quickSort(int[] arr, int low, int high){
-        if(low<high){
-            int pivoteIndex = partition(arr, low, high);
-
-            quickSort(arr, low, pivoteIndex-1);
-            quickSort(arr, pivoteIndex+1, high);
+        for(int i=0, index = 0; i<=max; i++){
+            while(count[i]-->0) arr[index++]=i;
         }
     }
+    public static void main(String [] args){
+        int [] arr = {4, 3, 2, 1, 4, 3, 2,1, 1, 2, 3, 4, 5};
 
-    public int partition(int[] arr, int low, int high){
-        // Right most element as pivote
-        int pivote = arr[high];
-
-        int i=low-1;
-
-        for(int j=low; j<high; j++){
-            if(arr[j]<pivote){
-                i++;
-
-                // Swapping element at i with element at j
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
-        }
-
-        // Swap the pivot element with the greater element at i+1
-        int temp = arr[i+1];
-        arr[i+1] = arr[high];
-        arr[high] = temp;
-
-        return i+1;
-    }
-
-    public static void main(String[] args) {
-        int[] arr = { 1, 3, 4, 2, 5 };
-
-        JavaDSA jd = new JavaDSA();
-        jd.quickSort(arr, 0, arr.length-1);
-
-        System.out.print("Sorted Array" + Arrays.toString(arr));
+        counting(arr);
+        System.out.println(Arrays.toString(arr));
     }
 }
